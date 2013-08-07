4aea50ce91475b618ead7cea7a19f9dd597bad95
baiyanwei <baiyanwei@example.com>
2013-08-07 22:56:51 +0800

    connect to tss

--- src/com/secpro/platform/core/exception/PlatformException.java
+++ src/com/secpro/platform/core/exception/PlatformException.java
@@ -4,12 +4,21 @@ public class PlatformException extends Exception {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -879646472897563183L;
+	private static final long serialVersionUID = 1424038216344067102L;
+	private String _exceptionMassage = null;
 
-	public PlatformException(String cause,Throwable able){
-		
+	public PlatformException(String cause, Throwable able) {
+		super(cause, able);
+		this._exceptionMassage = cause;
 	}
-	public PlatformException(String exceptionMassage){
-		
+
+	public PlatformException(String exceptionMassage) {
+		super(exceptionMassage);
+		this._exceptionMassage = exceptionMassage;
 	}
+	
+	public String getExceptionMassage() {
+		return _exceptionMassage;
+	}
+
 }
